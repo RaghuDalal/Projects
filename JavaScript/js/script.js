70890d6f5d5a531864d17c1b4eb7ea4b0693d840
Raghvender <raghvender002@gmail.com>
2020-09-30 11:15:53 +0530

    Started JS Function

---
+++ JavaScript/js/script.js
@@ -0,0 +1,23 @@
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
+
+
+
+function sing(song) {
+    console.log(song);
+}
+sing("Hello there");
+
+
+
+function multiply(a,b) {
+     return a * b;
+     
+ }
+ multiply(5, 10)
+
+
+//parameters(like a & B)
+//arguments(LIKE THE actualy values (5, 10))
