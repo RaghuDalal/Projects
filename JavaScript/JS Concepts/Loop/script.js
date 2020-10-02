5a1d4cd51b06427b14ca0e6b2d7edb69b192bfad
Raghvender <raghvender002@gmail.com>
2020-10-02 10:44:04 +0530

    Loops in JS

---
+++ JavaScript/JS Concepts/Loop/script.js
@@ -0,0 +1,20 @@
+var todo = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study js",
+    "eat healthy",
+    "stay fit"
+];
+
+for (var i=0; i < todo.length; i++ ) {
+    todo[i] = todo[i] + "!" ;
+}
+
+//while Loop
+
+var counterOne = 0;
+while (counterOne < 10){
+    console.log(counterOne);
+    counterOne++
+}
