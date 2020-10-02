0b4c917145ea300aa4912aeb346a2db2d0d0a9d3
Raghvender <raghvender002@gmail.com>
2020-10-02 11:02:54 +0530

    forEach Loop in JS

--- JavaScript/JS Concepts/Loop/script.js
+++ JavaScript/JS Concepts/Loop/script.js
@@ -7,14 +7,41 @@ var todo = [
     "stay fit"
 ];
 
+//for Loop (for adding ! in the end)
 for (var i=0; i < todo.length; i++ ) {
     todo[i] = todo[i] + "!" ;
 }
 
+//for loop (for logging todo)
+
+for (var i=0; i < todo.length; i++ ) {
+    console.log(todo[i], i);
+}
+
+//for each Method/Loop (for logging todo)
+
+todo.forEach(function(todo, i){
+    console.log(todo, i);
+})
+
+//More efficent way of using For Each Loop
+
+function logTodos(todo, i) {
+    console.log(todo, i);
+}
+todo.forEach(logTodos);
 //while Loop
 
 var counterOne = 0;
 while (counterOne < 10){
     console.log(counterOne);
-    counterOne++
+    counterOne++ ;
 }
+
+//do while Loop
+
+var counterTwo = 0
+do {
+    console.log("do while",counterTwo);
+    counterTwo++;
+} while (counterTwo < 10);
