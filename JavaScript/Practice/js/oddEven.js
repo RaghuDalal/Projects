dc7e67cb735e41a13969966d667dce8078439356
Raghvender <raghvender002@gmail.com>
2020-10-07 10:09:19 +0530

    JS Practice

---
+++ JavaScript/Practice/js/oddEven.js
@@ -0,0 +1,20 @@
+function find() {
+    var nmbr = document.getElementById('num').value;
+    var number = parseInt(nmbr);
+    if(number==0){
+        document.getElementById('change').innerHTML = "It's Zero";
+    }
+    else if (number % 2 == 0){
+        document.getElementById('change').innerHTML = "The number is Even";
+        
+    } 
+    
+    else if(number < 0){
+        document.getElementById('change').innerHTML = "Number is Negative";
+
+    }
+    else {
+        document.getElementById('change').innerHTML = "The number is odd";
+
+    }
+}
