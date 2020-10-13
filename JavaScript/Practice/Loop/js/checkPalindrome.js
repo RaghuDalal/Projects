ff5b3e9b03c6c6585ffeb877ba658e615400c0d0
Raghvender <raghvender002@gmail.com>
2020-10-13 11:41:41 +0530

    JS Check Palindrome

---
+++ JavaScript/Practice/Loop/js/checkPalindrome.js
@@ -0,0 +1,23 @@
+function check(n) {
+    let digit, result = 0;
+    while (n) {
+        digit = n % 10;
+        result = (result * 10) + digit;
+        n = n / 10 | 0;
+    }
+
+    return result;
+
+
+}
+
+function run() {
+    val = document.getElementById("num").value;
+    let rev = check(val);
+    if (rev==val){
+        document.getElementById("change").innerHTML = ("True");
+    }
+    else{
+        document.getElementById("change").innerHTML = ("False");
+    }
+}
