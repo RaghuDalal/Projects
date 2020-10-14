03c8b7f608e870ad99e721f8bc1517b54e93776b
Raghvender <raghvender002@gmail.com>
2020-10-14 15:54:44 +0530

    Beautify the Pattern JS

--- JavaScript/Practice/Pattern/js/pattern.js
+++ JavaScript/Practice/Pattern/js/pattern.js
@@ -1,270 +1,242 @@
-function rightAngleTriangle()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function rightAngleTriangle() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+'<span style="width:30px;">*<\span>   ';
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= row; col++) {
+
+            div = div + '*';
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "left";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "left";
+    document.getElementById('star').innerHTML = div;
+
 }
 
 
-function colTriangle()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function colTriangle() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ col;
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= row; col++) {
+
+            div = div + col;
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "left";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "left";
+    document.getElementById('star').innerHTML = div;
+
 }
 
 
-function rowTriangle()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function rowTriangle() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ row;
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= row; col++) {
+
+            div = div + row;
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "left";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "left";
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function squarePattern()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function squarePattern() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=number;col++)
-         {
-            
-            div=div+ number;
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= number; col++) {
+
+            div = div + number;
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function rowReverseTriangle()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function rowReverseTriangle() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ (row - col + 1 );
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= row; col++) {
+
+            div = div + (row - col + 1);
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function invertedNumber()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function invertedNumber() {
 
-  let div="";
-   
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
- 
-         for(let col=1; col<=number-row+1;col++)
-         {
-            
-            div=div+ (number-row+1 );
-         }
-    div=div+'<br>';
+    let div = "";
+
+
+    for (let row = 1; row <= number; row++) {
+
+        for (let col = 1; col <= number - row + 1; col++) {
+
+            div = div + (number - row + 1);
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "left";
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function mirrorNumber()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function mirrorNumber() {
 
-  let div="";
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
-         for (let space=1;space<=number-row;space++){
+    let div = "";
 
-            div=div+ ' ';
+    for (let row = 1; row <= number; row++) {
+        for (let space = 1; space <= number - row; space++) {
 
-         }
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ col;
-         }
-    div=div+'<br>';
+            div = div + ' ';
+
+        }
+        for (let col = 1; col <= row; col++) {
+
+            div = div + col;
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "end";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "end";
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function mirrorStars()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function mirrorStars() {
 
-  let div="";
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
-         for (let space=1;space<=number-row;space++){
+    let div = "";
 
-            div=div+ ' ';
+    for (let row = 1; row <= number; row++) {
+        for (let space = 1; space <= number - row; space++) {
 
-         }
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ "*";
-         }
-    div=div+'<br>';
+            div = div + ' ';
+
+        }
+        for (let col = 1; col <= row; col++) {
+
+            div = div + "*";
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "end";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "end";
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function halfDiamond()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function halfDiamond() {
 
-  let div="";
+    let number = parseInt(document.getElementById('num').value);
 
-    for( let row=1; row<=number; row++)
-    {
-         for (let space=1;space<=number-row;space++){
+    let div = "";
 
-            div=div+ '';
+    for (let row = 1; row <= number; row++) {
+        for (let space = 1; space <= number - row; space++) {
 
-         }
-         for(let col=1; col<=row;col++)
-         {
-            
-            div=div+ "*";
-         }
-    div=div+'<br>';
+            div = div + '';
+
+        }
+        for (let col = 1; col <= row; col++) {
+
+            div = div + "*";
+        }
+        div = div + '<br>';
 
     }
-    document.getElementById('star').style.textAlign= "center";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "center";
+    document.getElementById('star').innerHTML = div;
+
 }
 
-function Diamond()
-{
- 
-  let number= parseInt(document.getElementById('num').value);
+function Diamond() {
 
-  let div="";
+    let number = parseInt(document.getElementById('num').value);
+
+    let div = "";
 
 
-    let n1 = (number+1)/2
+    let n1 = (number + 1) / 2
 
-    
 
-    for( let row=1; row<=n1; row++){
-    
-         for (let space=1;space<=n1-row;space++){
 
-            div=div+ ' ';
+    for (let row = 1; row <= n1; row++) {
 
-         }
-         for(let star=1; star<=(2*row-1);star++)
-         {
-            
-            div=div+ ("*");
-         }
-    div=div+'<br>';
+        for (let space = 1; space <= n1 - row; space++) {
+
+            div = div + ' ';
+
+        }
+        for (let star = 1; star <= (2 * row - 1); star++) {
+
+            div = div + ("*");
+        }
+        div = div + '<br>';
     }
 
-let n2=number/2
-for (let row=1;row<=n2;row++){
-    for(let space2=1;space2<=row;space2++){
-        div=div+ ' ';
+    let n2 = number / 2
+    for (let row = 1; row <= n2; row++) {
+        for (let space2 = 1; space2 <= row; space2++) {
+            div = div + ' ';
 
+        }
+        star2 = number - 2 * row
+        for (star2; star2 > 0; star2--) {
+            div = div + ("*");
+
+        }
+        div = div + '<br>';
     }
-    star2=number-2*row
-    for (star2 ;star2 > 0;star2--){
-        div=div+ ("*");
 
-    }
-    div=div+'<br>';
-}
 
-    
-    document.getElementById('star').style.textAlign= "center";
-    document.getElementById('star').innerHTML= div;
-      
+    document.getElementById('star').style.textAlign = "center";
+    document.getElementById('star').innerHTML = div;
+
 }
