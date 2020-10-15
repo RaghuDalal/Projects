14ffadbcf626d121770f046ec647a478d0f6c2c9
Raghvender <raghvender002@gmail.com>
2020-10-15 14:34:22 +0530

    JS Assignments

---
+++ JavaScript/Practice/Loop/js/numToString.js
@@ -0,0 +1,46 @@
+function convert(n) {
+    const num = val;
+    const arrayOfDigits = Array.from(String(num), Number);
+    const result = [];
+
+    for (let element of arrayOfDigits) {
+        let nm = converter(element);
+        result.push(nm);
+    }
+
+    return result;
+
+}
+
+function converter(nmbr) {
+    if (nmbr == 0) {
+        return ("Zero");
+    } else if (nmbr == 1) {
+        return ("One");
+    } else if (nmbr == 2) {
+        return ("Two");
+    } else if (nmbr == 3) {
+        return ("Three");
+    } else if (nmbr == 4) {
+        return ("Four");
+    } else if (nmbr == 5) {
+        return ("Five");
+    } else if (nmbr == 6) {
+        return ("Six");
+    } else if (nmbr == 7) {
+        return ("Seven");
+    } else if (nmbr == 8) {
+        return ("Eight");
+    } else if(nmbr == 9){
+        return ("Nine");
+    } else{
+        return ("Please enter a valid Number")
+    }
+
+}
+
+
+function run() {
+    val = document.getElementById("num").value;
+    document.getElementById("change").innerHTML = convert(val)
+}
