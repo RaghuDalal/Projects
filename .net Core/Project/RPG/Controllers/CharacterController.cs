f7f082a68fe83bc19f19691f365461f94ff6a3c1
Raghvender <raghvender002@gmail.com>
2020-10-26 13:23:51 +0530

    Added few changes in .net core app

--- .net Core/Project/RPG/Controllers/CharacterController.cs
+++ .net Core/Project/RPG/Controllers/CharacterController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using RPG.Models;
 
 namespace RPG.Controllers
@@ -7,10 +9,21 @@ namespace RPG.Controllers
     [Route("[controller]")]
     public class CharacterController: ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{ID = 1,Name= "Sam"}
+        };
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+
+        public  IActionResult GetSingle(int ID)
+        {
+            return Ok(characters.FirstOrDefault(c=>c.ID==ID));
         }
     }
 }
--- .net Core/Project/RPG/Models/Character.cs
+++ .net Core/Project/RPG/Models/Character.cs
@@ -3,7 +3,7 @@ namespace RPG.Models
     public class Character
     {
         public int ID {get; set;}
-        public string Name {get; set;}
+        public string Name {get; set;}= "Raghu";
         public int HitPoints {get; set;} = 100;
         public int Strength {get; set;} = 10;
         public int Defence {get; set;} = 10;
