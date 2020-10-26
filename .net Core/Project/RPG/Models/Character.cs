32adb1851ca2a4ab990025c7a4715ab89c0b6788
Raghvender <raghvender002@gmail.com>
2020-10-26 11:43:32 +0530

    First API call

---
+++ .net Core/Project/RPG/Models/Character.cs
@@ -0,0 +1,14 @@
+namespace RPG.Models
+{
+    public class Character
+    {
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public int HitPoints {get; set;} = 100;
+        public int Strength {get; set;} = 10;
+        public int Defence {get; set;} = 10;
+        public int Intelligence {get; set;} = 10;
+        public RpgClass Class {get; set;} = RpgClass.Knight;
+
+    }
+}
