d051c9cd1b8989cbaf0d87f465abe778887e70c4
Raghvender <raghvender002@gmail.com>
2020-10-28 10:32:29 +0530

    Modifying the List JS

--- JavaScript/Practice/Array/js/list.js
+++ JavaScript/Practice/Array/js/list.js
@@ -1,11 +1,27 @@
 var student = []
+var studentList = {}
 
 function StudentInsert() {
 
     var name = document.getElementById('name').value;
+    var RollNo = document.getElementById('rollNo').value;
+    var Date = document.getElementById('date').value;
+    var MobileNo = document.getElementById('mobileNo').value;
 
-    student.push(name);
+    //student.push(name);
+
+    studentList.name = name;
+    studentList.rollno = RollNo;
+    studentList.date = Date;
+    studentList.mobileno = MobileNo;
+
+    student.push(studentList);
+
     document.getElementById('name').value = '';
+    document.getElementById('rollNo').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('mobileNo').value = '';
+
     StudentList();
 
 }
@@ -13,10 +29,20 @@ function StudentInsert() {
 function StudentUpdate() {
 
     var name = document.getElementById('name').value;
+    var rollNO = document.getElementById('rollNo').value;
+    var date = document.getElementById('date').value;
+    var mobileNo = document.getElementById('mobileNo').value;
+
+
     var index = document.getElementById('index').value; 
     student[index] = name; 
+
     document.getElementById('name').value = '';
+    document.getElementById('rollNo').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('mobileNo').value = '';
     document.getElementById('index').value = ''; 
+
     StudentList(); 
 }
 
@@ -34,14 +60,12 @@ function StudentById(index) {
     document.getElementById('btnInsert').style.display = 'none';
     document.getElementById('btnUpdate').style.display = 'block';
 
-
-
 }
 
 function StudentList() {
-    var data = '<table><tr><td>ID &nbsp </td> <td>&nbsp &nbsp &nbsp Name</td><td> &nbsp &nbsp &nbsp  Edit</td><td> &nbsp &nbsp &nbsp Delete</td></tr>';
+    var data = '<table> <tr>  <td>ID &nbsp </td>    <td> &nbsp &nbsp &nbsp Name </td>  <td> &nbsp &nbsp &nbsp Roll No </td>  <td> &nbsp &nbsp &nbsp Mobile No </td> <td> &nbsp &nbsp &nbsp Date </td> <td> &nbsp &nbsp &nbsp Edit </td>    <td> &nbsp &nbsp &nbsp Delete </td>  </tr>';
 
-    for (var index = 0; index < student.length; index++) {
+    for (var index = 0; index < studentList.length; index++) {
         data = data + '<tr> <td>' + (index + 1) + '</td>  <td>' + "&nbsp &nbsp &nbsp " + student[index] + '</td> ';
         data = data + ' <td>&nbsp &nbsp &nbsp <button onclick="StudentById(' + index + ')">Edit </button> </td>';
         data = data + '<td> &nbsp &nbsp &nbsp <button onclick="StudentDelete(' + index + ')">Delete </button> </td>';
@@ -53,6 +77,5 @@ function StudentList() {
 
     document.getElementById('btnInsert').style.display = 'block';
     document.getElementById('btnUpdate').style.display = 'none';
-
 
 }
