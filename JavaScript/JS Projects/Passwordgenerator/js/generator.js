6c2d70df16d0a5fef7fb8a70d5b2d457980e295f
Raghvender <raghvender002@gmail.com>
2020-10-28 16:34:56 +0530

    Fixed few problems

---
+++ JavaScript/JS Projects/Passwordgenerator/js/generator.js
@@ -0,0 +1,44 @@
+function loop(){
+    let length = parseInt(document.getElementById('len').value);
+    let randomNum = "";
+    let n = charset.length
+    for (var i = 0 ; i < length; ++i) {
+        randomNum += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById('result').innerHTML = randomNum;
+}
+
+function numOnly(){
+
+    charset = "0123456789";
+    loop();
+
+}
+
+function lowerCase(){
+
+    charset = "abcdefghijklmnopqrstuvwxyz";
+    loop();
+
+}
+
+function upperCase(){
+
+    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    loop();
+
+}
+
+function lowerUpper(){
+
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    loop();
+
+}
+
+function numAlpha(){
+
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    loop();
+
+}
