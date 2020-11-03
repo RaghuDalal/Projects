8c27bf8bb4949e454eae63820c7896490b4c8a60
Raghvender <raghvender002@gmail.com>
2020-11-03 11:29:06 +0530

    Modified API Fetch in JS

--- JavaScript/API - JS/js/script.js
+++ JavaScript/API - JS/js/script.js
@@ -1,16 +1,3 @@
-function run(){
-
-    let ID = parseInt(document.getElementById('id').value);
-    let Name = document.getElementById('name').value;
-    let Email = document.getElementById('email').value;
-    let PhoneNo = parseInt(document.getElementById('phoneNo').value);
-    let RollNo = parseInt(document.getElementById('rollNo').value);
-
-
-
-}
-
-
 
 function getData(){
     url = "http://localhost:54005/api/stduentapi/GetStudents/";
@@ -19,16 +6,18 @@ function getData(){
 
 
 function postData(){
-    url = "http://dummy.restapiexample.com/api/v1/create";
-    data = '{"name":"harglry347485945","salary":"123","age":"23"}'
+    let Name = document.getElementById('name').value;
+    let Email = document.getElementById('email').value;
+    let PhoneNo = parseInt(document.getElementById('phoneNo').value);
+    let RollNo = parseInt(document.getElementById('rollNo').value);
+    url = "http://localhost:54005/api/stduentapi/PostStudent";
+    data = {"name":""+Name+"","mobileNo":PhoneNo,"rollNo":RollNo,"emailid":""+Email+""}
     params = {
         method:'post',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: data
+        body:JSON.stringify(data)
     }
-    fetch(url, params).then(response=> response.json())
-    .then(data => console.log(data)
-    )
+    fetch(url, params).then(response=> response.text())
 }
