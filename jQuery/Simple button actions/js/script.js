ae033c3cafad97b7527be67766231cec869f63c7
Raghvender <raghvender002@gmail.com>
2020-11-05 16:16:20 +0530

    Started the jQuery Assignment

---
+++ jQuery/Simple button actions/js/script.js
@@ -0,0 +1,15 @@
+//Change Background color and Border Properties
+$(document).ready(function(){
+    $("#btn").click(function(){
+        $(".divToChange").toggleClass("red");
+    });
+
+});
+
+//Move the text
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        
+    });
+
+});
