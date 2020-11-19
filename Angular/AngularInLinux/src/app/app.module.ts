a5a85f1b5296c5ac50728d8e4a5421411a5e4551
Raghvender <raghvender002@gmail.com>
2020-11-19 11:56:11 +0530

    Added Interpolation in Angular

--- Angular/AngularInLinux/src/app/app.module.ts
+++ Angular/AngularInLinux/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Library to use 2 way Binding
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
