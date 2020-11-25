db1d3015e430e22ab482e9e40b1695506cc75535
Raghvender <raghvender002@gmail.com>
2020-11-25 16:20:15 +0530

    little changes

--- Angular/angularRouting/src/app/app.module.ts
+++ Angular/angularRouting/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { MasterComponent } from './master/master.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent }
-  
-  
 ];
 
 @NgModule({
